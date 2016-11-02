e20cde9db4e74856edaea9ca8622b480f973ba2f
Kyli Rouge <Kyli@BHStudios.org>
2016-11-01 22:22:52 -0400

    Added a helper class to abstract-away the thought of what type of number to use

---
+++ JVM/src/org/bh/tools/base/abstraction/Across Architectures.kt
@@ -0,0 +1,21 @@
+package org.bh.tools.base.abstraction
+
+
+/*
+ * Copyright BHStudios ©2016 BH-1-PS. Made for Snek.
+ *
+ * For when you wanna use the best stuff for this particular processor
+ *
+ * @author Kyli Rouge
+ * @since 2016-10-31
+ */
+
+/**
+ * The ideal type of integer
+ */
+typealias BHInt = Long
+
+/**
+ * The ideal type of fractional number
+ */
+typealias BHFloat = Double
