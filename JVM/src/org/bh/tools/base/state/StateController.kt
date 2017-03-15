17b8504599cb222a93991ee7bdc3aba593776517
Ben Leggiero <Ben@BHStudios.org>
2017-03-15 12:44:02 -0400

    Added mutation listener to StateController

--- JVM/src/org/bh/tools/base/state/StateController.kt
+++ JVM/src/org/bh/tools/base/state/StateController.kt
@@ -11,11 +11,12 @@ package org.bh.tools.base.state
  * @author Kyli Rouge
  * @since 2016-11-09
  */
-interface StateController<out StateType : State, ActionType> {
+interface StateController<out StateType : State, in ActionType> {
     /**
      * @return The current state in this controller
      */
     fun currentState(): StateType
+
 
     /**
      * A request to mutate the current state by using the given action. This might be ignored; the state might not
@@ -24,4 +25,22 @@ interface StateController<out StateType : State, ActionType> {
      * @param action The action that aims to mutate this controller's state
      */
     fun mutate(action: ActionType)
+
+
+    /**
+     * Adds the given state mutation listener to the listeners that will be invoked when the state mutates. Order of
+     * adding does not necessarily correspond to order of execution.
+     *
+     * @param stateMutationListener The listener to be notified when the state mutates
+     */
+    fun addStateMutationListener(stateMutationListener: StateMutationListener<StateType>)
+}
+
+
+
+/**
+ * Allows an object to listen for changes to a state
+ */
+interface StateMutationListener<in StateType: State> {
+    fun stateDidMutate(oldState: StateType, newState: StateType)
 }
