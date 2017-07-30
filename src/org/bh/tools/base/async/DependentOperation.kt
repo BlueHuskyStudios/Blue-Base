c12aae6584fd6e553858ae7a98041275731f5e86
Ben Leggiero <Ben@BHStudios.org>
2017-07-29 21:50:45 -0400

    Starting IDEA project again from scratch using existing code

--- src/org/bh/tools/base/async/DependentOperation.kt
+++ src/org/bh/tools/base/async/DependentOperation.kt
@@ -9,6 +9,7 @@ private var dependentOperationCount = 0
  * @author Kyli Rouge
  * @since 2017-01-20
  */
+@Deprecated("Not yet implemented", level = DeprecationLevel.HIDDEN)
 class DependentOperation(block: OperationBlock,
                          name: String = "Blue Base Dependent Operation #$dependentOperationCount",
                          var dependencies: List<Operation>) : Operation(block = block, name = name) {
