81e6a80deb4682c03d3188d6131f628fc483efcb
Ben Leggiero <BenLeggiero@Gmail.com>
2018-07-09 00:00:31 -0400

    Added predefined functions

---
+++ src/org/bh/tools/base/func/Predefined Functions.kt
@@ -0,0 +1,18 @@
+package org.bh.tools.base.func
+
+/*
+ * A collection of pre-defined functions, like mappers
+ *
+ * @author Ben Leggiero
+ * @since 2018-07-08
+ */
+
+
+typealias Function0<Return> = () -> Return
+typealias Function1<Arg1, Return> = (Arg1) -> Return
+typealias Function2<Arg1, Arg2, Return> = (Arg1, Arg2) -> Return
+typealias Function3<Arg1, Arg2, Arg3, Return> = (Arg1, Arg2, Arg3) -> Return
+
+
+typealias Mapper<OldElement, NewElement> = Function1<OldElement, NewElement>
+typealias FlatMapper<OldElement, NewElement> = Function1<OldElement, NewElement?>
